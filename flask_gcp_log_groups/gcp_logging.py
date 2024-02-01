434fdebc0f54138f7bd06a8c2ea6d586663e55d8
Christie Grinham <christiegrinham@gmail.com>
2024-02-01 15:46:30 +0000

    CRM-6156 Implement resource type and remote ip header

--- flask_gcp_log_groups/gcp_logging.py
+++ flask_gcp_log_groups/gcp_logging.py
@@ -2,41 +2,65 @@
 # Use of this source code is governed by the Apache 2.0
 # license that can be found in the LICENSE file.
 
-import logging
-import json
 import datetime
-
+import logging
 import time
 import os
-from flask import Flask
-from flask import request, Response, render_template, g, jsonify, current_app
-
 from google.cloud import logging as gcplogging
-from google.cloud.logging_v2.resource import Resource
+from flask import request, g
 
 from flask_gcp_log_groups.background_thread import BackgroundThreadTransport
+
+
 LOGGER = logging.getLogger()
 LOGGER.setLevel(logging.INFO)
-
 PROJECT = os.environ.get("GROUPED_LOGGING_GCP_PROJECT", os.environ.get("GOOGLE_CLOUD_PROJECT", ""))
 CLIENT = gcplogging.Client(project=PROJECT)
+
+REMOTE_IP_HEADER = os.environ.get("GROUPED_LOGGING_REMOTE_IP_HEADER")
+
 LOG_PREFIX = os.environ.get("GROUPED_LOGGING_LOG_PREFIX")
-if hasattr(os.environ, "GCP_LOG_USE_X_HTTP_CLOUD_CONTEXT"):
-    USE_X_HTTP_CLOUD_CONTEXT = os.environ.get("GCP_LOG_USE_X_HTTP_CLOUD_CONTEXT")
+TRANSPORT_PARENT = None
+if os.environ.get("GCP_LOG_USE_X_HTTP_CLOUD_CONTEXT"):
+    USE_X_HTTP_CLOUD_CONTEXT = os.environ.get("GCP_LOG_USE_X_HTTP_CLOUD_CONTEXT") or False
     TRANSPORT_PARENT = None
 else:
     USE_X_HTTP_CLOUD_CONTEXT = False
+
+if not USE_X_HTTP_CLOUD_CONTEXT:
     PARENT_LOG_NAME = f"{LOG_PREFIX}_request_log" if LOG_PREFIX else "request_log"
     TRANSPORT_PARENT = BackgroundThreadTransport(CLIENT, PARENT_LOG_NAME)
+
+CHILD_LOG_NAME = f"{LOG_PREFIX}_application" if LOG_PREFIX else "application"
+TRANSPORT_CHILD = BackgroundThreadTransport(CLIENT, CHILD_LOG_NAME)
+
+if os.environ.get("K_SERVICE"):
+    RESOURCE = gcplogging.Resource(
+        type='cloud_run_revision',
+        labels={
+            "service_name": os.environ.get("K_SERVICE"),
+            "revision_name": os.environ.get("K_REVISION"),
+            "configuration_name": os.environ.get("K_CONFIGURATION"),
+        }
+    )
+else:
+    RESOURCE = gcplogging.Resource(type='gae_app', labels={})
 
 
 class GCPHandler(logging.Handler):
 
-    def __init__(self, app, parentLogName='request', childLogName='application', 
-                traceHeaderName=None,labels=None, resource=None):
+    def __init__(
+        self,
+        app,
+        parentLogName='request',
+        childLogName='application',
+        traceHeaderName=None,
+        labels=None,
+        resource=None,
+    ):
         logging.Handler.__init__(self)
         self.app = app
-        self.labels=labels
+        self.labels = labels
         self.traceHeaderName = traceHeaderName
         labels = resource.get('labels', {}) if resource else {}
         if os.environ.get("K_SERVICE"):
@@ -59,19 +83,19 @@ class GCPHandler(logging.Handler):
         SEVERITY = record.levelname
 
         # if the current log is at a lower level than is setup, skip it
-        if (record_level < LOGGER.level):
+        if record_level < LOGGER.level:
             return
         if SEVERITY not in self.mLogLevels:
             self.mLogLevels[SEVERITY] = record_level
         TRACE = None
         SPAN = None
-        if (self.traceHeaderName in request.headers.keys()):
-          # trace can be formatted as "X-Cloud-Trace-Context: TRACE_ID/SPAN_ID;o=TRACE_TRUE"
-          rawTrace = request.headers.get(self.traceHeaderName).split('/')
-          TRACE = rawTrace[0]
-          TRACE = f"projects/{PROJECT}/traces/{TRACE}"
-          if ( len(rawTrace) > 1) :
-              SPAN = rawTrace[1].split(';')[0]
+        if self.traceHeaderName in request.headers.keys():
+            # trace can be formatted as "X-Cloud-Trace-Context: TRACE_ID/SPAN_ID;o=TRACE_TRUE"
+            rawTrace = request.headers.get(self.traceHeaderName).split('/')
+            TRACE = rawTrace[0]
+            TRACE = f"projects/{PROJECT}/traces/{TRACE}"
+            if len(rawTrace) > 1:
+                SPAN = rawTrace[1].split(';')[0]
 
         self.transport_child.send(
                 msg,
@@ -93,19 +117,18 @@ class GCPHandler(logging.Handler):
             gcp_handler.setLevel(logging.INFO)
             LOGGER.addHandler(gcp_handler)
 
-
         # always log the http_request@ default INFO
         @app.after_request
         def add_logger(response):
             TRACE = None
             SPAN = None
-            if (self.traceHeaderName in request.headers.keys()):
-              # trace can be formatted as "X-Cloud-Trace-Context: TRACE_ID/SPAN_ID;o=TRACE_TRUE"
-              rawTrace = request.headers.get(self.traceHeaderName).split('/')
-              TRACE = rawTrace[0]
-              TRACE = f"projects/{PROJECT}/traces/{TRACE}"
-              if ( len(rawTrace) > 1) :
-                SPAN = rawTrace[1].split(';')[0]
+            if self.traceHeaderName in request.headers.keys():
+                # trace can be formatted as "X-Cloud-Trace-Context: TRACE_ID/SPAN_ID;o=TRACE_TRUE"
+                rawTrace = request.headers.get(self.traceHeaderName).split('/')
+                TRACE = rawTrace[0]
+                TRACE = f"projects/{PROJECT}/traces/{TRACE}"
+                if len(rawTrace) > 1:
+                    SPAN = rawTrace[1].split(';')[0]
 
             # https://github.com/googleapis/googleapis/blob/master/google/logging/type/http_request.proto
             REQUEST = {
@@ -114,12 +137,12 @@ class GCPHandler(logging.Handler):
                 'status': response.status_code,
                 'responseSize': response.content_length,
                 'latency': g.request_time(),
-                'remoteIp': request.remote_addr,
-                'requestSize': request.content_length  
+                'remoteIp': (request.headers.get(REMOTE_IP_HEADER) if REMOTE_IP_HEADER else None) or request.remote_addr,
+                'requestSize': request.content_length
             }
 
             if 'user-agent' in request.headers:
-                REQUEST['userAgent'] = request.headers.get('user-agent') 
+                REQUEST['userAgent'] = request.headers.get('user-agent')
 
             if request.referrer:
                 REQUEST['referer'] = request.referrer
@@ -140,12 +163,12 @@ class GCPHandler(logging.Handler):
             if not USE_X_HTTP_CLOUD_CONTEXT:
                 self.transport_parent.send(
                     None,
-                    timestamp= datetime.datetime.utcnow(),
-                    severity = severity,
+                    timestamp=datetime.datetime.utcnow(),
+                    severity=severity,
                     resource=self.resource,
                     labels=self.labels,
                     trace=TRACE,
-                    span_id = SPAN,
+                    span_id=SPAN,
                     http_request=REQUEST)
 
             #response.headers['x-upstream-service-time'] = g.request_time()
